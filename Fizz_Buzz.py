ec0f8c8d6ef8ca36dc390efbc1825e073743cb08
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-07 08:57:13 +0000

    a few changes made

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -14,19 +14,24 @@
 import time
 program = 'GO'
 
-while program != 'STOP':
+def three_mod(num1):
+    return x % 3 == 0
+def five_mod(num2):
+    return x%5 == 0
+
+while program != 'stop':
     x = int(input('Input a number to check if it is fizz or buzz: '))
 
     for x in range(0,x+1):
 
-        if x%3 == 0 and x%5 == 0 and x != 0:
+        if three_mod(x) and five_mod(x) and x != 0:
             print('FizzBuzz')
             time.sleep(0.1)
-        elif x%3 == 0 and x != 0:
+        elif three_mod(x) and x != 0:
             print('POP')
             time.sleep(0.1)
 
-        elif x%5 == 0 and x != 0:
+        elif five_mod(x) and x != 0:
             print('TOC')
             time.sleep(0.1)
 
@@ -34,5 +39,5 @@ while program != 'STOP':
             print(x)
             time.sleep(0.1)
 
-    program = input('To stop game enter "STOP", otherwise hit "ENTER"')
+    program = input('To stop game enter "STOP", otherwise hit "ENTER"').lower()
 
